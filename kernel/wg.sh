ecdf77ca14baad66d561676231756f65d5e87bbb
azrim <mirzaspc@gmail.com>
2020-06-28 14:20:50 +0700

    Clean Up 3
    Signed-off-by: azrim <mirzaspc@gmail.com>

--- kernel/wg.sh
+++ kernel/wg.sh
@@ -12,7 +12,7 @@ cd "$WG"
 if ! [ -f wireguard-linux-compat-"$WGVER".tar.xz ]; then
     wget https://git.zx2c4.com/wireguard-linux-compat/snapshot/wireguard-linux-compat-"$WGVER".tar.xz 
 fi
-cd $KERNEL_DIR
+cd "$KERNEL_DIR"
 rm -rf net/wireguard && mkdir net/wireguard
 tar -C "net/wireguard" -xJf "$WG"/wireguard-linux-compat-"$WGVER".tar.xz --strip-components=2 wireguard-linux-compat-"$WGVER"/src
 git add net && git commit -am "net: wireguard: update wireguard to version $WGVER"
