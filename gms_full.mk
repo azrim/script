398282a232f21d61e5b1eafc276b700266abaa8b
Hiitagiii <mirzaspc@gmail.com>
2021-04-14 11:46:57 +0700

    Update gms_full.mk

--- gms_full.mk
+++ gms_full.mk
@@ -6,6 +6,7 @@ PRODUCT_PACKAGES += \
     DevicePolicyPrebuilt \
     GoogleContacts \
     GoogleContactsSyncAdapter \
+    GoogleDialer \
     GoogleTTS \
     LatinIMEGooglePrebuilt \
     LocationHistoryPrebuilt \
